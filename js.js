784e3bc60965b2325b6e8878b7e51a301f01b152
Tanya <vodilova@mail.ru>
2021-08-18 17:10:01 +0300

    Lesson 15-2

--- js.js
+++ js.js
@@ -1,72 +1,23 @@
 'use strict';
 
-const todoControlForm  = document.querySelector('.todo-control'),
-        headerInput = document.querySelector('.header-input'),
-        todoList = document.querySelector('.todo-list'),
-        todoCompleted = document.querySelector('.todo-completed'),
-        headerBtn = document.getElementById('add');
-  
-
-const todoData = JSON.parse(localStorage.getItem('toDoList')) || [];     
-
-const render = function() {
-    todoList.textContent = '',
-    todoCompleted.textContent = '';
-
-    todoData.forEach(function(item, index){
-        const toDoListElement = document.createElement('li');
-        toDoListElement.classList.add('todo-item');
-        toDoListElement.innerHTML = '<span class="text-todo">' + item.value + ' </span>' +
-        '<div class="todo-buttons">' + 
-            '<button class="todo-remove"></button>' + 
-            '<button class="todo-complete"></button>' +
-        '</div>';
-        
-        if (item.completed){
-            todoCompleted.append(toDoListElement);
-        } else {
-            todoList.append(toDoListElement);
-        }
-        
-        const btntodoComplete = toDoListElement.querySelector('.todo-complete'); 
-        
-        btntodoComplete.addEventListener('click', function(){
-            item.completed = !item.completed;
-            upDateLocalStorage();
-            render();
-        })        
-        
-        const btntodoRemove = toDoListElement.querySelector('.todo-remove'); 
-        
-        btntodoRemove.addEventListener('click', function(){
-            todoData.splice(index, 1);
-            upDateLocalStorage();
-            render();
-        });                
-    })
-
-       
-};
-
-todoControlForm.addEventListener('submit', function(event){
-    event.preventDefault();
-    if (headerInput.value.trim() === '') return;
-
-    todoData.push({
-        value: headerInput.value,
-        completed: false
-    });
-    
-    upDateLocalStorage();
-
-    headerInput.value = '';
-    
-    render();  
-});
-
-
-function upDateLocalStorage() {
-    localStorage.toDoList = JSON.stringify(todoData)
+class First {
+      hello (){
+          console.log("Привет я метод родителя!");
+        }   
+}
+class Second extends First {
+    hello (){
+        First1.hello();
+        console.log("А я наследуемый метод!");
+    }
 }
 
-render();
+const First1 = new First();
+const Second1 =  new Second();
+
+
+
+// Second1.hello1();
+
+Second1.hello();
+
